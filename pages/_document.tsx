88ab837d1c066c98f1f90304aa5193911d629ef7
matonny <onny.mat@gmail.com>
2022-06-06 13:44:34 +0100

    project setup

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+//`this file is rendered only once so it can't have dynamic content - kinda template for the page
+
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang='pl'>
+      <Head  />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
