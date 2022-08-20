3c6e8cc3d0227724652c1484390a26b22a4c1e21
Mateusz Wygonny <mateuszwygonny@Mateusz-Wygonny.local>
2022-08-20 14:26:43 +0200

    hold

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,32 @@
 import { ActiveLink } from "./ActiveLink";
 
 export const Header = () => {
-  const links = [["/", "Glowna"], ["/about", "About"], ["/products", "Products"]]
+  const links = [
+    ["/", "Glowna"],
+    ["/about", "About"],
+    ["/products", "Products"],
+  ];
   return (
-    <header className="max-w-md mx-auto w-full">
-      <nav className="bg-gray-700 text-white px-4 py-2">
-        {links.map((elem)=>{
-          return(
-            <ActiveLink 
-            href={elem[0]}
-            label={elem[1]}
-            key={elem[1]}
-            />
-          )
-        })
-      }
+    <header className="max-w-md p-4 mx-auto w-full flex items-center justify-between bg-gray-700">
+      <nav className="text-white px-4 py-2">
+        {links.map((elem) => {
+          return <ActiveLink href={elem[0]} label={elem[1]} key={elem[1]} />;
+        })}
       </nav>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 text-white"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+        strokeWidth={2}
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+        />
+      </svg>
     </header>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,6 @@ const DATA = {
   title: "Whatever",
   description:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam tempus nec dui pulvinar pharetra. Aenean et lacus ut metus sagittis porttitor vel et felis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut porta suscipit est.",
-  thumbnailUrl: "https://picsum.photos/id/1060/536/354",
   thumbnailAlt: "Barista nalewajacy kawe do Chemexa",
   rating: 4.5,
 };
@@ -17,9 +16,7 @@ const Home = () => {
   return (
     <div className="flex flex-col  min-h-screen">
       <Header />
-      <Main>
-        <ProductDetails data={DATA} />
-      </Main>
+      <Main></Main>
       <Footer />
     </div>
   );
