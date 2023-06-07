56263e195a8a5ecd95dec0b3b70b543bfe35320d
Mateusz Wygonny <onny.mat@gmail.com>
2023-06-07 16:26:15 +0200

    refactor: mapping through all inputs

--- components/Form/CheckoutAddressForm.tsx
+++ components/Form/CheckoutAddressForm.tsx
@@ -28,6 +28,14 @@ export const CheckoutAddressForm = () => {
       placeholder: "john.doe@gmail.com",
     },
     { name: "phone", label: "Phone number", placeholder: "123456789" },
+    {
+      name: "cardNumber",
+      label: "Card number",
+      placeholder: "1111222233334444",
+    },
+    { name: "cardCvc", label: "CVC", placeholder: "123" },
+    { name: "cardExpiry", label: "Expiry date", placeholder: "12/24" },
+    { name: "postalCode", label: "Postal Code", placeholder: "00-000" },
   ];
 
   return (
@@ -47,101 +55,6 @@ export const CheckoutAddressForm = () => {
               </div>
             );
           })}
-          <fieldset className="col-span-6">
-            <legend className="block text-sm font-medium text-gray-700">
-              Card Details
-            </legend>
-
-            <div className="mt-1 -space-y-px rounded-md bg-white shadow-sm">
-              <div>
-                <label htmlFor="CardNumber" className="sr-only">
-                  {" "}
-                  Card Number{" "}
-                </label>
-
-                <input
-                  type="text"
-                  id="CardNumber"
-                  {...register("cardNumber")}
-                  placeholder="Card Number"
-                  className="relative mt-1 w-full rounded-t-md border-gray-200 focus:z-10 sm:text-sm"
-                />
-              </div>
-
-              <div className="flex">
-                <div className="flex-1">
-                  <label htmlFor="CardExpiry" className="sr-only">
-                    {" "}
-                    Card Expiry{" "}
-                  </label>
-
-                  <input
-                    type="text"
-                    id="CardExpiry"
-                    {...register("cardExpiry")}
-                    placeholder="Expiry Date"
-                    className="relative w-full rounded-es-md border-gray-200 focus:z-10 sm:text-sm"
-                  />
-                </div>
-
-                <div className="-ms-px flex-1">
-                  <label htmlFor="CardCVC" className="sr-only">
-                    {" "}
-                    Card CVC{" "}
-                  </label>
-
-                  <input
-                    type="text"
-                    id="CardCVC"
-                    {...register("cardCvc")}
-                    placeholder="CVC"
-                    className="relative w-full rounded-ee-md border-gray-200 focus:z-10 sm:text-sm"
-                  />
-                </div>
-              </div>
-            </div>
-          </fieldset>
-
-          <fieldset className="col-span-6">
-            <legend className="block text-sm font-medium text-gray-700">
-              Billing Address
-            </legend>
-
-            <div className="mt-1 -space-y-px rounded-md bg-white shadow-sm">
-              <div>
-                <label htmlFor="Country" className="sr-only">
-                  Country
-                </label>
-
-                <select
-                  id="Country"
-                  className="relative w-full rounded-t-md border-gray-200 focus:z-10 sm:text-sm"
-                >
-                  <option>England</option>
-                  <option>Wales</option>
-                  <option>Scotland</option>
-                  <option>France</option>
-                  <option>Belgium</option>
-                  <option>Japan</option>
-                </select>
-              </div>
-
-              <div>
-                <label className="sr-only" htmlFor="PostalCode">
-                  {" "}
-                  ZIP/Post Code{" "}
-                </label>
-
-                <input
-                  type="text"
-                  id="PostalCode"
-                  {...register("postalCode")}
-                  placeholder="ZIP/Post Code"
-                  className="relative w-full rounded-b-md border-gray-200 focus:z-10 sm:text-sm"
-                />
-              </div>
-            </div>
-          </fieldset>
 
           <div className="col-span-6">
             <button className="block w-full rounded-md bg-black p-2.5 text-sm text-white transition hover:shadow-lg">
